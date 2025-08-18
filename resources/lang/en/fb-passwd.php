dd6d6f54885286c5bdb7c205cde72b21f9df7760
Morteza Masumi <morteza.masumi@gmail.com>
2025-08-18 05:21:01 +0330

    fix: minor

--- resources/lang/en/fb-passwd.php
+++ resources/lang/en/fb-passwd.php
@@ -3,7 +3,6 @@
 return [
     'title' => 'Change Password',
     'current_password' => 'Current password',
-    'change_password' => 'Change password',
     'notification' => 'Password updated successfully',
     'action' => 'Update password',
     'user_menu' => 'Change password',
