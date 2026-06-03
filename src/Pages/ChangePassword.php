8dc840388372ff3de77acfe41c7dfecb8e6de9c0
Morteza Masumi <morteza.masumi@gmail.com>
2026-06-03 13:14:32 +0330

    fix: update

--- src/Pages/ChangePassword.php
+++ src/Pages/ChangePassword.php
@@ -26,8 +26,6 @@ use Mortezamasumi\FbPasswd\Middleware\ForcePasswordChangeMiddleware;
 use Filament\Pages\PageConfiguration;
 use Throwable;
 
-// just comment
-
 use function Filament\Support\is_app_url;
 
 class ChangePassword extends BaseEditProfile
